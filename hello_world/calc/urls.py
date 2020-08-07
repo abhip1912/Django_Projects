16db1fdd84c2774914f2b2f1d2ce7a55b5e5cfb3
abhip1912 <abhipatil1912@gmail.com>
2020-08-07 21:38:36 +0530

    New Hello world

--- hello_world/calc/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.do_nothing, name="do_nothing"),
-    path('about/', views.home, name="home"),
-    # path('about/', views.about, name="about")
-    # path('about/', views.about, name="about")
-]
